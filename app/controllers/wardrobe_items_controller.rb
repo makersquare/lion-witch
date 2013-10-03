3ce71b5086174889097ac9564af49f0ddd2ef6e6
Michael Ornellas <mike@themakersquare.com>
2013-10-03 14:29:02 -0500

    Revert "rolling back to day-4 start"
    This reverts commit 9968b110df9ed0f35f449b82fb738c65d9957ffb.

--- app/controllers/wardrobe_items_controller.rb
+++ app/controllers/wardrobe_items_controller.rb
@@ -16,19 +16,39 @@ class WardrobeItemsController < ApplicationController
     @wardrobe_item = WardrobeItem.new
   end
 
+  def edit
+    @wardrobe_item = WardrobeItem.find(params[:id])
+  end
+
   def create
     @wardrobe_item = WardrobeItem.new(wardrobe_item_params)
 
     if @wardrobe_item.save
-      redirect_to @wardrobe_item
+      redirect_to @wardrobe_item, notice: "Wardrobe Item was successfully created"
     else
       render action: 'new'
     end
   end
 
-  private
-  
-  def wardrobe_item_params
-    params.require(:wardrobe_item).permit(:garment, :label, :season, :color)
+  def update
+    @wardrobe_item = WardrobeItem.find(params[:id])
+
+    if @wardrobe_item.update(wardrobe_item_params)
+      redirect_to @wardrobe_item, notice: "Wardrobe Item was successfully updated"
+    else
+      render action: 'edit'
+    end
   end
+
+  def destroy
+    @wardrobe_item = WardrobeItem.find(params[:id])
+    @wardrobe_item.destroy
+
+    redirect_to wardrobe_items_url
+  end
+
+  private
+    def wardrobe_item_params
+      params.require(:wardrobe_item).permit(:garment, :label, :season, :color)
+    end
 end
