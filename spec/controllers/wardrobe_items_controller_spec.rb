1f62ef924048dd358ec3da63827ce6a4d9d49a52
leejaew <lee.jaewang@gmail.com>
2013-10-03 11:31:55 -0500

    add test on the update action is whether or not update will actually update a WardrobeItem

--- spec/controllers/wardrobe_items_controller_spec.rb
+++ spec/controllers/wardrobe_items_controller_spec.rb
@@ -80,11 +80,18 @@ describe WardrobeItemsController do
       put :update, {:id => wardrobe_item, :wardrobe_item => valid_attributes}
       expect(assigns(:wardrobe_item)).to eq(wardrobe_item)
     end
-    
+
     it "redirects to the wardrobe_item" do
       wardrobe_item = WardrobeItem.create valid_attributes
       put :update, {:id => wardrobe_item, :wardrobe_item => valid_attributes}
       expect(response).to redirect_to(wardrobe_item)
+    end
+
+    it "updates the requested wardrobe_item" do
+      wardrobe_item = WardrobeItem.create valid_attributes
+      expect_any_instance_of(WardrobeItem).to receive(:update).with({ "garment" => "shoes" })
+
+      put :update, {:id => wardrobe_item, :wardrobe_item => { "garment" => "shoes" }}
     end
   end
 
